561ed1d8b80d210d0c2037e933c4a81120f2246b
tobstern <tobias.philipp@email.uni-kiel.de>
2024-12-04 10:18:15 +0100

    init day04

---
+++ days/day04.py
@@ -0,0 +1,40 @@
+import time
+import os
+import numpy as np
+
+
+def part1(input_str):
+    start_time = time.time()
+    filename = os.path.basename(__file__)
+    day_num = filename.replace("day", "").replace(".py", "").strip()
+    print(f"Day {day_num}, Part 1:")
+    # Implement the logic here
+
+    word_map = []
+    for i, line in enumerate(input_str.splitlines()):
+        # print(list(line))
+        word_map.append(list(line))
+
+    np_word_map = np.asarray(word_map)
+    print(np_word_map)
+
+    result = ""
+    print(f"Part 1 result is: {result}")
+
+    end_time = time.time()
+    print(f"Part 1 execution time: {end_time - start_time} seconds")
+
+
+def part2(input_str):
+    start_time = time.time()
+    filename = os.path.basename(__file__)
+    day_num = filename.replace("day", "").replace(".py", "").strip()
+    print(f"Day {day_num}, Part 1:")
+    # Implement the logic here
+    # Your part2 logic here
+
+    result = ""
+    print(f"Part 2 result is: {result}")
+
+    end_time = time.time()
+    print(f"Part 2 execution time: {end_time - start_time} seconds")
