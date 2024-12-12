57e8e18f4c9c6cac8b4bf1bd412bfb44988cd6b8
tobstern <tobiasphilipp11@gmail.com>
2024-12-12 22:49:28 +0100

    ini 12

---
+++ days/day12.py
@@ -0,0 +1,31 @@
+import time
+import os
+
+
+def part1(input_str):
+    start_time = time.time()
+    filename = os.path.basename(__file__)
+    day_num = filename.replace("day", "").replace(".py", "").strip()
+    print(f"Day {day_num}, Part 1:")
+    # Implement the logic here
+
+    result = ""
+    print(f"Part 1 result is: {result}")
+
+    end_time = time.time()
+    print(f"Part 1 execution time: {end_time - start_time} seconds")
+
+
+def part2(input_str):
+    start_time = time.time()
+    filename = os.path.basename(__file__)
+    day_num = filename.replace("day", "").replace(".py", "").strip()
+    print(f"Day {day_num}, Part 2:")
+    # Implement the logic here
+    # Your part2 logic here
+
+    result = ""
+    print(f"Part 2 result is: {result}")
+
+    end_time = time.time()
+    print(f"Part 2 execution time: {end_time - start_time} seconds")
