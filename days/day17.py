8564b97c00e55916f266715607819cb594ead7fc
tobstern <tobiasphilipp11@gmail.com>
2024-12-23 16:02:36 +0100

    fifnished day17 part2 finally - thx HyperNeutrino

--- days/day17.py
+++ days/day17.py
@@ -37,6 +37,10 @@ def part1(input_str):
     print(regis)
     print(instrus)
 
+    regis["A"] = 100
+    regis["B"] = 0
+    regis["C"] = 0
+
     # start opcode:
     out = []
     ip = 0
@@ -45,10 +49,10 @@ def part1(input_str):
 
         # check current and next
         cn = instrus[ip]
-        print(f"ip-cn={ip}")
+        # print(f"ip-cn={ip}")
         ip += 1
         combo_op = instrus[ip]
-        print(f"ip-comb_op={ip}")
+        # print(f"ip-comb_op={ip}")
         ip += 1
 
         # print()
@@ -101,15 +105,60 @@ def part1(input_str):
     print(f"Part 1 execution time: {end_time - start_time} seconds")
 
 
+import re
+
+
 def part2(input_str):
     start_time = time.time()
     filename = os.path.basename(__file__)
     day_num = filename.replace("day", "").replace(".py", "").strip()
     print(f"Day {day_num}, Part 2:")
     # Implement the logic here
-    # Your part2 logic here
 
+    # regis_str, instrus_str = input_str.split("\n\n")
+    # regis = {}
+    # reg_ch = ["A", "B", "C"]
+    # for i, line in enumerate(regis_str.strip().splitlines()):
+    #     # print(line)
+    #     regis[reg_ch[i]] = int(line.strip().split(": ")[1])
+
+    # instrus = list(map(int, instrus_str.split(": ")[1].strip().split(",")))
+    inp = list(map(int, re.findall(r"\d+", input_str)[3:]))
+
+    print(inp)
+    # exit()
+    # print(regis)
+    # print(instrus)
+
+    # start opcode:
     result = 0
+
+    # for Aval in range(37_000_000_000_000, 38_000_000_000_000, 1): 266_932_601_404_433
+    # THX to HyperNeutrino!
+    # So, I did reverse engineer my program and then applied HyperNeutrinos recursive function,
+    # for finding the next possible A register value - based on matching it to the program value.
+    def find(program, ans):
+        if program == []:
+            return ans
+
+        for t in range(8):
+            a = ans << 3 | t
+
+            b = a % 8
+            b = b ^ 3
+            c = a >> b
+            # a = a >> 3 # this operation can be anywhere from here till end (so it moved first op here).
+            b = b ^ 4
+
+            b = b ^ c
+
+            if b % 8 == program[-1]:
+                sub = find(program[:-1], a)
+                if sub == None:
+                    continue
+                return sub
+
+    result = find(inp, 0)
     print(f"Part 2 result is: {result}")
 
     end_time = time.time()
